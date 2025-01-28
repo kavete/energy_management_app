6d4da1b603708a289dd7dd3f5cb5cdf90c5329ea
kavete <kavetebrian@gmail.com>
2025-01-28 11:44:54 +0300

    Set up the front end, changed some models

--- e_app/admin.py
+++ e_app/admin.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 # Register your models here.
 admin.site.site_header = "Energy Management Application"
 admin.site.index_title = "Applications"
+admin.site.index_title = "Welcome to the Energy Management Application Dashboard"
 
 from .models import PowerSource, Load, ConsumptionData
 
