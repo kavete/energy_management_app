ffe7f1620784269d719ffb2e77e889d4e2ba304b
kavete <kavetebrian@gmail.com>
2025-02-03 12:31:20 +0300

    Linked some backend data to the frontend

--- e_app/views.py
+++ e_app/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
-# Create your views here.
+from .models import PowerSource, ConsumptionData, Load, Notification
+from django.db.models import Sum
 
 def home(request):
-    return  render(request, 'home.html')
+    power_sources = PowerSource.objects.all()
+    consumption_data = ConsumptionData.objects.all()
+    total_energy_consumed = consumption_data.aggregate(Sum('power_consumed')) ['power_consumed__sum'] or 0
+    loads = Load.objects.all()
+    # load_consumption_data_total_per_day = loads.aggregate(Sum(''))
+    notifications = Notification.objects.all()
+    context = {
+        "power_sources" : power_sources,
+        "consumption_data" : consumption_data,
+        "loads": loads,
+        "total_energy_consumed": total_energy_consumed,
+        'notifications': notifications,
+    }
+    return  render(request, 'home.html', context)
